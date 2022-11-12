edbe581e2739c6cbbd5026285b97f01c25d0281d
Jean Brigonnet <jeanbrigo@gmail.com>
2022-11-12 16:20:38 -0600

    Budget table showing

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express'); // backend framework
+const app = express();
+const budgets = require("./models/budgets")
+app.use("/static", express.static("public"));
+const PORT = 3000;
+
+// Index Route
+app.get('/budgets', (req,res)=>{
+    res.render('index.ejs', 
+    {
+        allBudget:budgets
+    });
+});
+
+// Show Route
+
+// app.get("/budgets/:index", (req,res)=>{
+//     res.render("show.ejs");
+// });
+
+// New Route
+
+
+// Create Route
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
